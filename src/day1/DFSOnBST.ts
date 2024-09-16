e6990437a14d085a912d3f4d925eb36a163a9a42
villander <nousvillander@gmail.com>
2024-09-15 21:55:46 -0300

    DFS on Binary Search Tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,22 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
+}
 
+function search(
+    currentNode: BinaryNode<number> | null,
+    needle: number,
+): boolean {
+    if (!currentNode) {
+        return false;
+    }
+
+    if (currentNode.value === needle) {
+        return true;
+    }
+
+    if (currentNode.value < needle) {
+        return search(currentNode.right, needle);
+    } else {
+        return search(currentNode.left, needle);
+    }
 }
